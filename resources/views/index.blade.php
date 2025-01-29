1caec2533c68b86f43b1dd3a0d63cf0ae7691fc0
lucas schneider <lucaseduschneiderengs@gmail.com>
2025-01-29 02:31:23 -0300

    feat adicionando controller e criando a view inicial

---
+++ resources/views/index.blade.php
@@ -0,0 +1,20 @@
+@extends('templates.template')
+
+@section('content')
+    <h1>Vagas</h1>
+    <table>
+  <thead>
+    <tr>
+      <th>id</th>
+      <th>Titulo</th>
+      <th>Cargo</th>
+      <th>Salario</th>
+      <th>Descricao</th>
+      <th>Acao</th>
+    </tr>
+  </thead>
+  <tbody>
+  
+  </tbody>
+</table>
+@endsection
--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,7 @@
 <?php
 
-use Illuminate\Support\Facades\Route;
+//use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\VagasController;
 
-Route::get('/', function () {
-    return view('welcome');
-});
+Route::get('/vagas', [VagasController::class, 'index']);
+
